f53ae0e4958cdb7384d4adafa24c03303c350594
vineethsankre <lsvineethsankre@gmail.com>
2023-04-18 20:25:23 +0530

    Fetched latest ami

--- main.tf
+++ main.tf
@@ -81,3 +81,21 @@ resource "aws_security_group" "my_sg" {
     Name = "${var.env_prefix}-sg"
   }
 }
+#
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+output "aws_ami_id" {
+  value = data.aws_ami.latest-amazon-linux-image.id
+}
+
