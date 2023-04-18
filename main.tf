d528ce4700ed2239d6849a4e7cc041b0d75d7754
vineethsankre <lsvineethsankre@gmail.com>
2023-04-18 19:39:04 +0530

    Created vpc,subnet,rtb,igw

--- main.tf
+++ main.tf
@@ -1,24 +1,47 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
-
+# Defining variables
 variable vpc_cidr_block {}
+variable subnet_cidr_block {}
 variable env_prefix {}
+variable avail_zone{}
 
 # Create a VPC
 resource "aws_vpc" "my_vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
     Name = "${var.env_prefix}-vpc"
+  }
+}
+# Create a subnet
+resource "aws_subnet" "my_subnet-1" {
+  vpc_id = aws_vpc.my_vpc.id
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.avail_zone
+  tags = {
+    Name = "${var.env_prefix}-subnet-1"
+  }
+}
+
+#Creating a route table for subnet
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+  tags = {
+    Name = "${var.env_prefix}-rtb"
+  }
+}
+#Creating internet gatewat
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "${var.env_prefix}-igw"
   }
 }
